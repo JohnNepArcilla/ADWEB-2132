71e4b3a8d93cc731707ba5f8d03b3b4254fa9570
JohnNepArcilla <83747280+JohnNepArcilla@users.noreply.github.com>
2022-02-03 22:29:28 +0800

    Add files via upload

--- Prelims/app.component.spec.ts
+++ Prelims/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,17 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'binding-lab-act'`, () => {
+  it(`should have as title 'binding-data'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('binding-lab-act');
+    expect(app.title).toEqual('binding-data');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('binding-lab-act app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('binding-data app is running!');
   });
-  
 });
